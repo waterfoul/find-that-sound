02d0fb0313887da09388f30a0b4a3a7d3d05c24f
Andrew Basore <basor1aj@cmich.edu>
2017-01-30 14:50:17 -0500

    implement fileUpload

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
 
 
 function addSoundEle() {
-	var soundPanel = $('<div style="top: 20px; left: 20px" class="panel panel-default sound-panel"><div class="crosshair">+</div>Stuff</div>');
+	var soundPanel = $('<div style="top: 20px; left: 20px" class="panel panel-default sound-panel"><div class="crosshair">+</div><input type="file" onchange="fileUpload(event)"/></div>');
 
 	soundPanel.draggable({ handle: ".crosshair", drag: onMove });
 
@@ -15,5 +15,18 @@ function onMove(event) {
 	var target = event.target;
 	var X = event.pageX;
 	var Y = event.pageY;
-	console.log(target, X, Y);
+	// console.log(target, X, Y);
+}
+
+function fileUpload(event){
+	var file    = event.srcElement.files[0];
+  var reader  = new FileReader();
+
+  reader.addEventListener("load", function () {
+    console.log(reader.result);
+  }, false);
+
+  if (file) {
+    reader.readAsDataURL(file);
+  }
 }
