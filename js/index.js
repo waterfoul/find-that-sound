c414160b88024a3a2a69ceea4394690aac59250c
Aaron Aichlmayr <waterfoul@gmail.com>
2017-01-30 14:26:02 -0600

    The person can move now

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
 $(document).ready(() => {
 	$('#add-btn').on('click', addSoundEle);
+
+	$('#person').draggable({ drag: (event) => {
+		var target = event.target;
+		playerX = (event.pageX - 200)/100;
+		playerZ = (event.pageY - 200)/100;
+
+		Howler.pos(playerX, 1, playerZ);
+	} });
 });
 
 var soundPanels = [];
+
+var playerX = 0;
+var playerZ = 0;
 
 
 function addSoundEle() {
@@ -68,6 +79,7 @@ function fileUpload(event){
 function play(event) {
 	var panelId = $(event.srcElement.parentNode).data('id');
 	soundPanels[panelId].howl.play();
+	soundPanels[panelId].howl.pos(soundPanels[panelId].X, 1, soundPanels[panelId].Z);
 }
 
 function pause(event) {
